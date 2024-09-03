853529219940f86fd4c2612611048bef43525184
KennethTheCode <117273823+KennethTheCode@users.noreply.github.com>
2024-09-03 16:25:17 +0800

    updated

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -4,25 +4,23 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="bg-stone-100 border-b-[1px] border-grey"> {/* navbar main color */}
-    <nav className="flex justify-between items-center w-[50%] ml-[34%]"> {/* navbar move to left and color 2 */}
-      <div>
-        <ul className="flex items-center gap-[2vw] text-1xl font-light"> {/* ul design */}
-         <img class="w-[180px]" src="../component/confi.png"></img>
-          <li className="text-black hover:text-gray-400 transition-colors duration-300">
-            <Link to="/">Main</Link>
-          </li>
-          <li className="text-black hover:text-gray-400 transition-colors duration-300">
-            <Link to="/sign">Sign</Link>
-          </li>
-          <li className="text-black hover:text-gray-400 transition-colors duration-300">
-            <Link to="/Functions">Functions</Link>
-          </li>
-           <button className="">Sign Up</button>
-        </ul>
-      </div>
-      
-  
-     </nav>
+      <nav className="flex justify-between items-center w-[200%] ml-[33%]"> {/* navbar move to left and color 2 */}
+        <div>
+          <ul className="flex items-center gap-[2vw] text-1xl font-light"> {/* ul design */}
+            <img className="w-[180px]" src="src/component/confi.png" alt="Logo" /> {/* Use the correct path */}
+            <li className="text-black hover:text-gray-400 transition-colors duration-300">
+              <Link to="/">Main</Link>
+            </li>
+            <li className="text-black hover:text-gray-400 transition-colors duration-300">
+              <Link to="/sign">Sign</Link>
+            </li>
+            <li className="text-black hover:text-gray-400 transition-colors duration-300">
+              <Link to="/Functions">Functions</Link>
+            </li>
+            <button className="">Sign Up</button>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
