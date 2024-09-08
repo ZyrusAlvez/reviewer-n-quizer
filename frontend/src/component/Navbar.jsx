820b478a2eaedae8408ea164f8416cf9b5e4a8a5
KennethTheCode <117273823+KennethTheCode@users.noreply.github.com>
2024-09-09 01:03:04 +0800

    updated

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="bg-stone-100 border-b-[1px] border-grey"> {/* navbar main color */}
-      <nav className="flex justify-between items-center w-[200%] ml-[33%]"> {/* navbar move to left and color 2 */}
+      <nav className="flex justify-between items-center w-[50%] ml-[33%]"> {/* navbar move to left and color 2 */}
         <div>
           <ul className="flex items-center gap-[2vw] text-1xl font-light"> {/* ul design */}
             <img className="w-[180px]" src="src/component/confi.png" alt="Logo" /> {/* Use the correct path */}
--- frontend/src/component/main/ChooseImport.jsx
+++ frontend/src/component/main/ChooseImport.jsx
@@ -1,25 +1,47 @@
 import React from "react";
+import { FaYoutube } from "react-icons/fa";
+import { MdPictureAsPdf } from "react-icons/md";
+import  { CgNotes } from "react-icons/cg";
+
+
 import { useNavigate } from "react-router-dom";
 
-const chooseImport = ({ divImportDisplay }) => {
+
+const ChooseImport = ({ divImportDisplay }) => {
   const navigate = useNavigate();
 
   return (
-    <div
-      style={{
-        display: divImportDisplay,
-        height: "60vh",
-        width: "70vw",
-        position: "absolute",
-        background: "lightblue",
-      }}
-      onClick={(e) => e.stopPropagation()}
-    >
-      <button onClick={() => navigate("/notes")}>Notes</button>
-      <button onClick={() => navigate("/yturl")}>Youtube URL</button>
-      <button>PDF</button>
+    <div className={`absolute h-[35vh] w-[60vw] bg-stone-200 border-[3px] border-gray-200 rounded-lg flex justify-around items-center ${divImportDisplay === 'block' ? 'block' : 'hidden'}`}
+  onClick={(e) => e.stopPropagation()}>
+
+  <div className="rounded-lg w-[23%] h-[90%] flex flex-col justify-around items-center border-b-1">
+    <div className=" h-[80%] w-[94%] m-2">
+      <p className="text-[14rem] text-sky-600 hover:text-stone-700 transition-colors duration-300"><CgNotes className="mr-2 color-white"/></p>
     </div>
+    <button className="mb-2 bg-yellow-500 text-white py-2 px-20 rounded hover:bg-gray-900 transition duration-300"
+      onClick={() => navigate("/notes")}> Notes </button>
+  </div>
+
+  <div className="rounded-lg w-[23%] h-[90%] flex flex-col justify-around items-center border-b-1">
+   <div className=" h-[80%] w-[94%] m-2">
+     <p className="text-[14rem] text-red-600 hover:text-stone-700 transition-colors duration-300"><FaYoutube className="mr-2" /></p>
+    </div>
+    <button className="bg-yellow-500 text-white py-2 px-16 rounded mb-2 hover:bg-gray-900 transition duration-300 flex items-center"
+      onClick={() => navigate("/yturl")}>
+       YouTube
+    </button>
+  </div>
+
+  <div className="rounded-lg w-[23%] h-[90%] flex flex-col justify-around items-center border-b-1">
+   <div className=" h-[80%] w-[94%] m-2">
+     <p className="text-[14rem] text-green-600 hover:text-stone-700 transition-colors duration-300"><MdPictureAsPdf className="mr-2" /></p>
+    </div>
+    <button className="bg-yellow-500 font-bold border rounded text-white py-2 px-20 mb-2 hover:bg-green-700 transition duration-300 flex items-center">
+       PDF
+    </button>
+  </div>
+</div>
   );
 };
 
-export default chooseImport;
+export default ChooseImport;
